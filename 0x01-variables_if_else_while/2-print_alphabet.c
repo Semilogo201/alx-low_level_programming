b28c90137a59be1144089cde35a4753d06fd0803
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2023-10-06 06:33:15 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,24 +2,19 @@
 
 /**
  * main - Prints the alphabet.
-
  *
-* Return: Always 0 (Success)
-
+ * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-	 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	 int i;
-	  for (i = 0; i < 26; i++)
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-        {
+	for (i = 0; i < 26; i++)
+	{
 		putchar(alp[i]);
-
-        }
-	   putchar('\n');
-
-        return (0);
-
+	}
+	putchar('\n');
+	return (0);
 }
+
